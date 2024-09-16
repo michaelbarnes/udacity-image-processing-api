b2bc737ec81d0cfb9ce9341fa3b7d2cd2fd3d12d
Mike Barnes <michael@journeyapps.com>
2024-09-15 21:37:00 -0600

    refactor: Updated logger.ts

--- src/middleware/logger.ts
+++ src/middleware/logger.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 
 export const logger = (req: Request, res: Response, next: () => void) => {
 	console.log(
-		`A new request has been submitted from ${req.ip} to path ${req.path}`,
+		`[${new Date().toUTCString()}] A new request has been submitted from ${req.ip} to path ${req.method}: ${req.path} Query: ${JSON.stringify(req.query)} Params: ${JSON.stringify(req.params)} Body: ${JSON.stringify(req.body)}`,
 	);
 	next();
 };
